d2d69f03cf4e7958e5f99999f3224ebe1483a270
Gabriel santana leite <111404839+gabesantana@users.noreply.github.com>
2022-10-31 21:29:08 -0300

    Create classesabstratas.java

---
+++ classesabstratas.java
@@ -0,0 +1,19 @@
+
+public class Main 
+{
+
+  public static void main(String[] args)
+    {
+        conta c= new Conta();
+    }
+}
+abstract class Conta {  }
+class Juridica extends Conta {  } 
+class Fisica {  }
+
+
+
+/* conceito de uso de classe abstrata
+pode ser usado quando uma classe deve ser herdada mas não 
+instecionada 
+
